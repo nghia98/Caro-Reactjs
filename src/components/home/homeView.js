0027b55e10ea8eb98d111dd9297262566d2e07a8
dnnghia98 <mteenzz@gmail.com>
2019-10-23 20:55:45 +0700

    Tạo các trang đăng nhập đăng kí, trang chủ dùng react-boostrap

---
+++ src/components/home/homeView.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Card, Button} from 'react-bootstrap'
+import Game from '../game/Game'
+
+const homeView = () => {
+
+    function logOut() {
+        localStorage.setItem('token', null);
+        window.location.href = '/login';
+    }
+
+    return (
+        <div>
+            <Card>
+                <Card.Header>Featured</Card.Header>
+                <Button variant="primary" onClick={() => logOut()}>Đăng xuất</Button>   
+            </Card>
+            <Game /> 
+        </div>
+    )
+}
+
+
+
+export default homeView;
+
